ae7f722e377d92bfb927b81384ab6f2a5da0c075
Linh <46288798+s272936@users.noreply.github.com>
2019-12-05 19:23:38 -0500

    update

---
+++ aSimplified21LinhH/aSimplified21LinhH/aSimplified21Form.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace aSimplified21LinhH
+{
+    partial class frmASimplified21
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.mniExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.mniNewGame = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnHit = new System.Windows.Forms.Button();
+            this.btnStand = new System.Windows.Forms.Button();
+            this.picUCard2 = new System.Windows.Forms.PictureBox();
+            this.picUCard = new System.Windows.Forms.PictureBox();
+            this.picDCard = new System.Windows.Forms.PictureBox();
+            this.picDCard2 = new System.Windows.Forms.PictureBox();
+            this.picDCard3 = new System.Windows.Forms.PictureBox();
+            this.picUCard3 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picUCard2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picUCard)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picDCard)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picDCard2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picDCard3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picUCard3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mniExit,
+            this.mniNewGame});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1006, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // mniExit
+            // 
+            this.mniExit.Name = "mniExit";
+            this.mniExit.Size = new System.Drawing.Size(37, 20);
+            this.mniExit.Text = "Exit";
+            // 
+            // mniNewGame
+            // 
+            this.mniNewGame.Name = "mniNewGame";
+            this.mniNewGame.Size = new System.Drawing.Size(77, 20);
+            this.mniNewGame.Text = "New Game";
+            // 
+            // btnHit
+            // 
+            this.btnHit.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnHit.Location = new System.Drawing.Point(773, 276);
+            this.btnHit.Name = "btnHit";
+            this.btnHit.Size = new System.Drawing.Size(119, 53);
+            this.btnHit.TabIndex = 1;
+            this.btnHit.Text = "Hit";
+            this.btnHit.UseVisualStyleBackColor = true;
+            this.btnHit.Click += new System.EventHandler(this.BtnHit_Click);
+            // 
+            // btnStand
+            // 
+            this.btnStand.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStand.Location = new System.Drawing.Point(773, 357);
+            this.btnStand.Name = "btnStand";
+            this.btnStand.Size = new System.Drawing.Size(119, 52);
+            this.btnStand.TabIndex = 2;
+            this.btnStand.Text = "Stand";
+            this.btnStand.UseVisualStyleBackColor = true;
+            this.btnStand.Click += new System.EventHandler(this.BtnStand_Click);
+            // 
+            // picUCard2
+            // 
+            this.picUCard2.Image = global::aSimplified21LinhH.Properties.Resources.Back;
+            this.picUCard2.Location = new System.Drawing.Point(249, 359);
+            this.picUCard2.Name = "picUCard2";
+            this.picUCard2.Size = new System.Drawing.Size(121, 181);
+            this.picUCard2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picUCard2.TabIndex = 3;
+            this.picUCard2.TabStop = false;
+            this.picUCard2.Click += new System.EventHandler(this.PicUCard2_Click_1);
+            // 
+            // picUCard
+            // 
+            this.picUCard.Image = global::aSimplified21LinhH.Properties.Resources.Back;
+            this.picUCard.Location = new System.Drawing.Point(87, 357);
+            this.picUCard.Name = "picUCard";
+            this.picUCard.Size = new System.Drawing.Size(121, 181);
+            this.picUCard.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picUCard.TabIndex = 4;
+            this.picUCard.TabStop = false;
+            this.picUCard.Click += new System.EventHandler(this.PicUCard2_Click);
+            // 
+            // picDCard
+            // 
+            this.picDCard.Image = global::aSimplified21LinhH.Properties.Resources.Back;
+            this.picDCard.Location = new System.Drawing.Point(87, 148);
+            this.picDCard.Name = "picDCard";
+            this.picDCard.Size = new System.Drawing.Size(121, 181);
+            this.picDCard.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picDCard.TabIndex = 5;
+            this.picDCard.TabStop = false;
+            // 
+            // picDCard2
+            // 
+            this.picDCard2.Image = global::aSimplified21LinhH.Properties.Resources.Back;
+            this.picDCard2.Location = new System.Drawing.Point(249, 148);
+            this.picDCard2.Name = "picDCard2";
+            this.picDCard2.Size = new System.Drawing.Size(121, 181);
+            this.picDCard2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picDCard2.TabIndex = 6;
+            this.picDCard2.TabStop = false;
+            // 
+            // picDCard3
+            // 
+            this.picDCard3.Image = global::aSimplified21LinhH.Properties.Resources.Back;
+            this.picDCard3.Location = new System.Drawing.Point(414, 148);
+            this.picDCard3.Name = "picDCard3";
+            this.picDCard3.Size = new System.Drawing.Size(121, 181);
+            this.picDCard3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picDCard3.TabIndex = 7;
+            this.picDCard3.TabStop = false;
+            // 
+            // picUCard3
+            // 
+            this.picUCard3.Image = global::aSimplified21LinhH.Properties.Resources.Back;
+            this.picUCard3.Location = new System.Drawing.Point(414, 359);
+            this.picUCard3.Name = "picUCard3";
+            this.picUCard3.Size = new System.Drawing.Size(121, 181);
+            this.picUCard3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picUCard3.TabIndex = 8;
+            this.picUCard3.TabStop = false;
+            // 
+            // frmASimplified21
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1006, 719);
+            this.Controls.Add(this.picUCard3);
+            this.Controls.Add(this.picDCard3);
+            this.Controls.Add(this.picDCard2);
+            this.Controls.Add(this.picDCard);
+            this.Controls.Add(this.picUCard);
+            this.Controls.Add(this.picUCard2);
+            this.Controls.Add(this.btnStand);
+            this.Controls.Add(this.btnHit);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "frmASimplified21";
+            this.Text = "Simplified 21";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picUCard2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picUCard)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picDCard)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picDCard2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picDCard3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picUCard3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem mniExit;
+        private System.Windows.Forms.ToolStripMenuItem mniNewGame;
+        private System.Windows.Forms.Button btnHit;
+        private System.Windows.Forms.Button btnStand;
+        private System.Windows.Forms.PictureBox picUCard2;
+        private System.Windows.Forms.PictureBox picUCard;
+        private System.Windows.Forms.PictureBox picDCard;
+        private System.Windows.Forms.PictureBox picDCard2;
+        private System.Windows.Forms.PictureBox picDCard3;
+        private System.Windows.Forms.PictureBox picUCard3;
+    }
+}
+
---
+++ aSimplified21LinhH/aSimplified21LinhH/aSimplified21Form.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace aSimplified21LinhH
+{
+    public partial class frmASimplified21 : Form
+    {
+        public frmASimplified21()
+        {
+            InitializeComponent();
+            // Call function
+            CardsDown();
+        }
+
+        private void CardsDown()
+        {
+            // Create a function that set cards to back cover
+            picUCard2.Image = Properties.Resources.Back;
+            picUCard.Image = Properties.Resources.Back;
+            picUCard3.Image = Properties.Resources.Back;
+
+            picDCard.Image = Properties.Resources.Back;
+            picDCard2.Image = Properties.Resources.Back;
+            picDCard3.Image = Properties.Resources.Back;
+        }
+
+
+        // goal is to display 2 cards
+        private void BtnHit_Click(object sender, EventArgs e)
+        {
+            // Call DealCard Function
+        }
+
+        private void BtnStand_Click(object sender, EventArgs e)
+        {
+            // Disable hit and stay button
+
+            // If user total is less than 17....
+            // Call CalculateTotal
+            // Otherwise... Call DealCard Function
+        }
+
+    }
+}
