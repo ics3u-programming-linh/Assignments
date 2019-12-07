14a4a74fe63bf156aed3dd5b752be637540acee5
Linh <46288798+s272936@users.noreply.github.com>
2019-12-06 19:30:46 -0500

    update

--- v2SimplifiedLinhH/v2SimplifiedLinhH/v2SimplifiedForm.Designer.cs
+++ v2SimplifiedLinhH/v2SimplifiedLinhH/v2SimplifiedForm.Designer.cs
@@ -36,18 +36,25 @@
             this.picUCard3 = new System.Windows.Forms.PictureBox();
             this.picUCard2 = new System.Windows.Forms.PictureBox();
             this.picUCard = new System.Windows.Forms.PictureBox();
+            this.grbScore = new System.Windows.Forms.GroupBox();
+            this.lblDealerScore = new System.Windows.Forms.Label();
+            this.lblDealer = new System.Windows.Forms.Label();
+            this.lblPlayerScore = new System.Windows.Forms.Label();
+            this.lblPlayer = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.picDCard3)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.picDCard2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.picDCard)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.picUCard3)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.picUCard2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.picUCard)).BeginInit();
+            this.grbScore.SuspendLayout();
             this.SuspendLayout();
             // 
             // btnStay
             // 
             this.btnStay.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnStay.Location = new System.Drawing.Point(629, 141);
+            this.btnStay.Location = new System.Drawing.Point(480, 326);
             this.btnStay.Name = "btnStay";
             this.btnStay.Size = new System.Drawing.Size(99, 43);
             this.btnStay.TabIndex = 16;
@@ -58,7 +65,7 @@
             // btnHit
             // 
             this.btnHit.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnHit.Location = new System.Drawing.Point(629, 82);
+            this.btnHit.Location = new System.Drawing.Point(480, 267);
             this.btnHit.Name = "btnHit";
             this.btnHit.Size = new System.Drawing.Size(99, 42);
             this.btnHit.TabIndex = 15;
@@ -68,6 +75,7 @@
             // 
             // picDCard3
             // 
+            this.picDCard3.Image = global::v2SimplifiedLinhH.Properties.Resources.Back;
             this.picDCard3.Location = new System.Drawing.Point(335, 239);
             this.picDCard3.Name = "picDCard3";
             this.picDCard3.Size = new System.Drawing.Size(100, 145);
@@ -77,6 +85,7 @@
             // 
             // picDCard2
             // 
+            this.picDCard2.Image = global::v2SimplifiedLinhH.Properties.Resources.Back;
             this.picDCard2.Location = new System.Drawing.Point(205, 239);
             this.picDCard2.Name = "picDCard2";
             this.picDCard2.Size = new System.Drawing.Size(100, 145);
@@ -86,6 +95,7 @@
             // 
             // picDCard
             // 
+            this.picDCard.Image = global::v2SimplifiedLinhH.Properties.Resources.Back;
             this.picDCard.Location = new System.Drawing.Point(72, 239);
             this.picDCard.Name = "picDCard";
             this.picDCard.Size = new System.Drawing.Size(100, 145);
@@ -95,6 +105,7 @@
             // 
             // picUCard3
             // 
+            this.picUCard3.Image = global::v2SimplifiedLinhH.Properties.Resources.Back;
             this.picUCard3.Location = new System.Drawing.Point(335, 67);
             this.picUCard3.Name = "picUCard3";
             this.picUCard3.Size = new System.Drawing.Size(100, 145);
@@ -104,6 +115,7 @@
             // 
             // picUCard2
             // 
+            this.picUCard2.Image = global::v2SimplifiedLinhH.Properties.Resources.Back;
             this.picUCard2.Location = new System.Drawing.Point(205, 67);
             this.picUCard2.Name = "picUCard2";
             this.picUCard2.Size = new System.Drawing.Size(100, 145);
@@ -121,11 +133,77 @@
             this.picUCard.TabIndex = 9;
             this.picUCard.TabStop = false;
             // 
+            // grbScore
+            // 
+            this.grbScore.Controls.Add(this.lblDealerScore);
+            this.grbScore.Controls.Add(this.lblDealer);
+            this.grbScore.Controls.Add(this.lblPlayerScore);
+            this.grbScore.Controls.Add(this.lblPlayer);
+            this.grbScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.grbScore.Location = new System.Drawing.Point(609, 267);
+            this.grbScore.Name = "grbScore";
+            this.grbScore.Size = new System.Drawing.Size(143, 85);
+            this.grbScore.TabIndex = 17;
+            this.grbScore.TabStop = false;
+            // 
+            // lblDealerScore
+            // 
+            this.lblDealerScore.AutoSize = true;
+            this.lblDealerScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDealerScore.Location = new System.Drawing.Point(6, 53);
+            this.lblDealerScore.Name = "lblDealerScore";
+            this.lblDealerScore.Size = new System.Drawing.Size(60, 24);
+            this.lblDealerScore.TabIndex = 5;
+            this.lblDealerScore.Text = "Score";
+            // 
+            // lblDealer
+            // 
+            this.lblDealer.AutoSize = true;
+            this.lblDealer.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDealer.Location = new System.Drawing.Point(68, 53);
+            this.lblDealer.Name = "lblDealer";
+            this.lblDealer.Size = new System.Drawing.Size(65, 24);
+            this.lblDealer.TabIndex = 6;
+            this.lblDealer.Text = "Dealer";
+            // 
+            // lblPlayerScore
+            // 
+            this.lblPlayerScore.AutoSize = true;
+            this.lblPlayerScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPlayerScore.Location = new System.Drawing.Point(6, 29);
+            this.lblPlayerScore.Name = "lblPlayerScore";
+            this.lblPlayerScore.Size = new System.Drawing.Size(60, 24);
+            this.lblPlayerScore.TabIndex = 7;
+            this.lblPlayerScore.Text = "Score";
+            // 
+            // lblPlayer
+            // 
+            this.lblPlayer.AutoSize = true;
+            this.lblPlayer.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPlayer.Location = new System.Drawing.Point(68, 29);
+            this.lblPlayer.Name = "lblPlayer";
+            this.lblPlayer.Size = new System.Drawing.Size(62, 24);
+            this.lblPlayer.TabIndex = 8;
+            this.lblPlayer.Text = "Player";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.Location = new System.Drawing.Point(190, 12);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(99, 36);
+            this.btnStart.TabIndex = 18;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.BtnStart_Click);
+            // 
             // frmV2Simplified
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.grbScore);
             this.Controls.Add(this.btnStay);
             this.Controls.Add(this.btnHit);
             this.Controls.Add(this.picDCard3);
@@ -142,6 +220,8 @@
             ((System.ComponentModel.ISupportInitialize)(this.picUCard3)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.picUCard2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.picUCard)).EndInit();
+            this.grbScore.ResumeLayout(false);
+            this.grbScore.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -156,6 +236,12 @@
         private System.Windows.Forms.PictureBox picUCard3;
         private System.Windows.Forms.PictureBox picUCard2;
         private System.Windows.Forms.PictureBox picUCard;
+        private System.Windows.Forms.GroupBox grbScore;
+        private System.Windows.Forms.Label lblDealerScore;
+        private System.Windows.Forms.Label lblDealer;
+        private System.Windows.Forms.Label lblPlayerScore;
+        private System.Windows.Forms.Label lblPlayer;
+        private System.Windows.Forms.Button btnStart;
     }
 }
 
--- v2SimplifiedLinhH/v2SimplifiedLinhH/v2SimplifiedForm.cs
+++ v2SimplifiedLinhH/v2SimplifiedLinhH/v2SimplifiedForm.cs
@@ -12,12 +12,237 @@ namespace v2SimplifiedLinhH
 {
     public partial class frmV2Simplified : Form
     {
+        
+        // Create a list for deck of cards and card values
+        List<Image> listDeckOfCards = new List<Image>();
+        List<int> listDeckOfValues = new List<int>();
+
+        // Declare variables and constants
+        Random randomNumberGenerator = new Random();
+        int randomNumber, playerScore, dealerScore, userTotal, dealerTotal;
+
+        const int MIN_NUM = 1;
+        const int MAX_NUM = 11;
+        const int BLACKJACK = 21;
+
         public frmV2Simplified()
         {
             InitializeComponent();
+            // Call function/procedures
+            CreateDeck();
+            CreateValues();
+
+            // disable hit and stay
+            btnHit.Enabled = false;
+            btnStay.Enabled = false;
+            
+        }
+        private void CreateDeck()
+        {
+            // populate the list with 52 cards
+            listDeckOfCards.Insert(0, Properties.Resources.AC);
+            listDeckOfCards.Insert(1, Properties.Resources.AD);
+            listDeckOfCards.Insert(2, Properties.Resources.AH);
+            listDeckOfCards.Insert(3, Properties.Resources.AS);
+
+            listDeckOfCards.Insert(4, Properties.Resources._2C);
+            listDeckOfCards.Insert(5, Properties.Resources._2D);
+            listDeckOfCards.Insert(6, Properties.Resources._2H);
+            listDeckOfCards.Insert(7, Properties.Resources._2S);
+
+            listDeckOfCards.Insert(8, Properties.Resources._3C);
+            listDeckOfCards.Insert(9, Properties.Resources._3D);
+            listDeckOfCards.Insert(10, Properties.Resources._3H);
+            listDeckOfCards.Insert(11, Properties.Resources._3S);
+
+            listDeckOfCards.Insert(12, Properties.Resources._4C);
+            listDeckOfCards.Insert(13, Properties.Resources._4D);
+            listDeckOfCards.Insert(14, Properties.Resources._4H);
+            listDeckOfCards.Insert(15, Properties.Resources._4S);
+
+            listDeckOfCards.Insert(16, Properties.Resources._5C);
+            listDeckOfCards.Insert(17, Properties.Resources._5D);
+            listDeckOfCards.Insert(18, Properties.Resources._5H);
+            listDeckOfCards.Insert(19, Properties.Resources._5S);
+
+            listDeckOfCards.Insert(20, Properties.Resources._6C);
+            listDeckOfCards.Insert(21, Properties.Resources._6D);
+            listDeckOfCards.Insert(22, Properties.Resources._6H);
+            listDeckOfCards.Insert(23, Properties.Resources._6S);
+
+            listDeckOfCards.Insert(24, Properties.Resources._7C);
+            listDeckOfCards.Insert(25, Properties.Resources._7D);
+            listDeckOfCards.Insert(26, Properties.Resources._7H);
+            listDeckOfCards.Insert(27, Properties.Resources._7S);
+
+            listDeckOfCards.Insert(28, Properties.Resources._8C);
+            listDeckOfCards.Insert(29, Properties.Resources._8D);
+            listDeckOfCards.Insert(30, Properties.Resources._8H);
+            listDeckOfCards.Insert(31, Properties.Resources._8S);
+
+            listDeckOfCards.Insert(32, Properties.Resources._9C);
+            listDeckOfCards.Insert(33, Properties.Resources._9D);
+            listDeckOfCards.Insert(34, Properties.Resources._9H);
+            listDeckOfCards.Insert(35, Properties.Resources._9S);
+
+            listDeckOfCards.Insert(36, Properties.Resources._10C);
+            listDeckOfCards.Insert(37, Properties.Resources._10D);
+            listDeckOfCards.Insert(38, Properties.Resources._10H);
+            listDeckOfCards.Insert(39, Properties.Resources._10S);
+
+            listDeckOfCards.Insert(40, Properties.Resources.JC);
+            listDeckOfCards.Insert(41, Properties.Resources.JD);
+            listDeckOfCards.Insert(42, Properties.Resources.JH);
+            listDeckOfCards.Insert(43, Properties.Resources.JS);
+
+            listDeckOfCards.Insert(44, Properties.Resources.KC);
+            listDeckOfCards.Insert(45, Properties.Resources.KD);
+            listDeckOfCards.Insert(46, Properties.Resources.KH);
+            listDeckOfCards.Insert(47, Properties.Resources.KS);
+
+            listDeckOfCards.Insert(48, Properties.Resources.QC);
+            listDeckOfCards.Insert(49, Properties.Resources.QD);
+            listDeckOfCards.Insert(50, Properties.Resources.QH);
+            listDeckOfCards.Insert(51, Properties.Resources.QS);
         }
 
-        // In the beginning generate two user cards
+        private void CreateValues()
+        {
+            // populate list with card values
+            listDeckOfValues.Add(1);
+            listDeckOfValues.Add(1);
+            listDeckOfValues.Add(1);
+            listDeckOfValues.Add(1);
+
+            listDeckOfValues.Add(2);
+            listDeckOfValues.Add(2);
+            listDeckOfValues.Add(2);
+            listDeckOfValues.Add(2);
+
+            listDeckOfValues.Add(3);
+            listDeckOfValues.Add(3);
+            listDeckOfValues.Add(3);
+            listDeckOfValues.Add(3);
+
+            listDeckOfValues.Add(4);
+            listDeckOfValues.Add(4);
+            listDeckOfValues.Add(4);
+            listDeckOfValues.Add(4);
+
+            listDeckOfValues.Add(4);
+            listDeckOfValues.Add(4);
+            listDeckOfValues.Add(4);
+            listDeckOfValues.Add(4);
+
+            listDeckOfValues.Add(5);
+            listDeckOfValues.Add(5);
+            listDeckOfValues.Add(5);
+            listDeckOfValues.Add(5);
+
+            listDeckOfValues.Add(6);
+            listDeckOfValues.Add(6);
+            listDeckOfValues.Add(6);
+            listDeckOfValues.Add(6);
+
+            listDeckOfValues.Add(7);
+            listDeckOfValues.Add(7);
+            listDeckOfValues.Add(7);
+            listDeckOfValues.Add(7);
+
+            listDeckOfValues.Add(8);
+            listDeckOfValues.Add(8);
+            listDeckOfValues.Add(8);
+            listDeckOfValues.Add(8);
+
+            listDeckOfValues.Add(9);
+            listDeckOfValues.Add(9);
+            listDeckOfValues.Add(9);
+            listDeckOfValues.Add(9);
+
+            listDeckOfValues.Add(10);
+            listDeckOfValues.Add(10);
+            listDeckOfValues.Add(10);
+            listDeckOfValues.Add(10);
+
+            listDeckOfValues.Add(10);
+            listDeckOfValues.Add(10);
+            listDeckOfValues.Add(10);
+            listDeckOfValues.Add(10);
+
+            listDeckOfValues.Add(10);
+            listDeckOfValues.Add(10);
+            listDeckOfValues.Add(10);
+            listDeckOfValues.Add(10);
+        }
+
+        private void GenerateRandomNumber()
+        {
+            // generate a random number
+            randomNumber = randomNumberGenerator.Next(0, listDeckOfCards.Count() - 1);
+        }
+
+        private int DealCard(ref PictureBox aPictureBox)
+        {
+            // declare variables
+            int cardValue = -1;
+
+            // if there are less than 6 cards, create new deck
+            if (listDeckOfCards.Count() <= 6)
+            {
+                CreateDeck();
+                MessageBox.Show("You ran out of cards. Deck is reshuffling.");
+            }
+            else
+            {
+                // generate a random number
+                randomNumber = randomNumberGenerator.Next(0, listDeckOfCards.Count() - 1);
+
+                // set the card to a picture from the listBox at a randomIndex
+                aPictureBox.Image = listDeckOfCards[randomNumber];
+
+                // get value of card
+                cardValue = listDeckOfValues[randomNumber];
+
+                // remove the deck of cards and values from their own list
+                listDeckOfCards.RemoveAt(randomNumber);
+                listDeckOfValues.Remove(randomNumber);
+            }
+
+            // return value
+            return cardValue;
+
+        }
+
+        private void BtnStart_Click(object sender, EventArgs e)
+        {
+            // Set all images to the back of the card
+            picUCard3.Image = Properties.Resources.Back;
+            picDCard.Image = Properties.Resources.Back;
+            picDCard2.Image = Properties.Resources.Back;
+            picDCard3.Image = Properties.Resources.Back;
+
+            // hide label
+            picUCard3.Hide();
+
+            // Disable/Enable buttons
+            btnStart.Enabled = false;
+            btnHit.Enabled = true;
+            btnStay.Enabled = true;
+
+            // declare variables
+            int uValue, uValue2;
+
+            // display two randomly generated cards
+            uValue = DealCard(ref this.picUCard);
+            uValue2 = DealCard(ref this.picUCard2);
+
+            // calculate player score
+            playerScore = uValue + uValue2;
+
+            // Convert from int to string
+            lblPlayerScore.Text = Convert.ToString(playerScore);
+        }
+
         private void BtnHit_Click(object sender, EventArgs e)
         {
 
